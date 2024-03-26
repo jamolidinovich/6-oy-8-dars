75288d0f9e5fd8879a489c9fa5e7c47b5a148743
anazirjonov <anazirjonov10@gmail.com>
2024-03-26 20:09:23 +0500

    weather

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from "react";
+import WeatherApp from "./componets/WeatherApp/WeatherApp";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App"></div>
+      <WeatherApp />
+    </>
+  );
+}
+
+export default App;
